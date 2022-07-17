dfcd22f610a3577293f0823e582f79f1341ef385
KeiBlokhin <keiblokhin@gmail.com>
2022-07-17 22:33:26 +0900

    initial commit

---
+++ components/Card.js
@@ -0,0 +1,48 @@
+import MuiCard from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Image from 'next/image'
+
+const styles = {
+  card: {
+    maxWidth: '384px',
+    padding: 0,
+  },
+  btn: {
+    textTransform: 'capitalize',
+  },
+}
+const Card = () => {
+  return (
+    <MuiCard sx={styles.card}>
+      <CardMedia>
+        {' '}
+        <Image src='/img/js-course.png' alt='me' width='382' height='342' />
+      </CardMedia>
+      <CardContent>
+        <Typography variant='h5'>
+          Web Development & Code 101 - Full Free Course [2022]
+        </Typography>
+        <Typography variant='body1'>
+          Learn programming in JS language, HTML, and CSS. Create a Web
+          Application made in React JS. The path to the final project includes
+          many assignments, code exercises, and challenges.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button sx={styles.btn}>
+          <Image src='/img/udemy-logo.png' alt='me' width='30' height='30' />
+          View On Udemy
+        </Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import Card from '../components/Card'
+
+const index = () => {
+  return (
+    <div>
+      Hello monkey
+      <Button variant='contained'>Kiss me</Button>
+      <Card />
+    </div>
+  )
+}
+
+export default index
